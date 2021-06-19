ea013c38de882808cec25d9da460707b5405b7f3
Aleksei <aleksei.golenishev5@gmail.com>
2021-06-19 12:15:46 +0300

    изменение в роутере URL host

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  default_url_options host: "https://finalnyi-proiekt-aleksisweb.rubycourse0121.repl.co/"
+  default_url_options host: "https://https://super-qrcode.herokuapp.com/"
   root 'qr_codes#new'
   get '/contacts', to: 'pages#contact', as: :contact
   get '/qr_codes', to:  'qr_codes#qr_code', as: :qr_codes 
